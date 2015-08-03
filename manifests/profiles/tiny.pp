a302785a9cfb440b4d909caa74829469fff87203
ronen <narkisr@gmail.com>
2015-08-04 00:13:35 +0300

    introducing profiles

---
+++ manifests/profiles/tiny.pp
@@ -0,0 +1,23 @@
+# Tiny first gen pi profile
+class pi::profiles::tiny {
+  include pi::ssh
+  include pi::duckdns
+  include pi::wlan
+  include pi::tty
+  include pi::shell
+  include pi::swap
+  include pi::mpsyt
+  include pi::bootlog
+  include pi::ipv6
+
+  exec{'noop scheduler':
+    command => "/bin/sed -i 's/deadline/noop/g' /boot/cmdline.txt",
+    user    => 'root',
+    onlyif  => '/usr/bin/test -f /boot/cmdline.txt'
+  }
+
+  package{'mlocate':
+    ensure  => present
+  }
+
+}
