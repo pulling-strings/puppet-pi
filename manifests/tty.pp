99b8c8a919f60ff9c43f5e9aa30dc8790c0b3fbf
ronen <narkisr@gmail.com>
2016-03-28 14:08:27 +0300

    multi version support in tty

--- manifests/tty.pp
+++ manifests/tty.pp
@@ -1,15 +1,19 @@
 # tty disabling
 class pi::tty {
+  if $::operatingsystemrelease == '8.0' {
 
-  exec{'remove extra ttys':
-    command => "sed -i '/[2-6]:23:respawn:\\/sbin\\/getty 38400 tty[2-6]/s%^%#%g' /etc/inittab",
-    user    => 'root',
-    path    => ['/usr/bin','/bin',]
+  } else {
+    exec{'remove extra ttys':
+      command => "sed -i '/[2-6]:23:respawn:\\/sbin\\/getty 38400 tty[2-6]/s%^%#%g' /etc/inittab",
+      user    => 'root',
+      path    => ['/usr/bin','/bin',]
+    }
+
+    exec{'disable gettty on serial line':
+      command => "sed -i '/T0:23:respawn:\\/sbin\\/getty -L ttyAMA0 115200 vt100/s%^%#%g' /etc/inittab",
+      user    => 'root',
+      path    => ['/usr/bin','/bin',]
+    }
   }
 
-  exec{'disable gettty on serial line':
-    command => "sed -i '/T0:23:respawn:\\/sbin\\/getty -L ttyAMA0 115200 vt100/s%^%#%g' /etc/inittab",
-    user    => 'root',
-    path    => ['/usr/bin','/bin',]
-  }
 }
