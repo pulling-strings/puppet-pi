ef1432c4f99524ec77a35e4ed42ea42ff4cca3da
ronen <narkisr@gmail.com>
2017-12-25 22:35:52 +1100

    pi desktop clean

---
+++ manifests/desktop.pp
@@ -0,0 +1,17 @@
+# Desktop cleanup and setup
+class pi::desktop {
+  $libreoffice = ['libreoffice-base-core', 'libreoffice-common', 'libreoffice-core',
+                  'libreoffice-gtk', 'libreoffice-math', 'libreoffice-style-elementary',
+                  'libreoffice-style-galaxy', 'libreoffice-writer', 'libreoffice-calc']
+
+  package{$libreoffice:
+    ensure  => absent
+  }
+
+  $development = ['geany', 'wolfram-engine', 'bluej', 'minecraft-pi',
+                  'greenfoot', 'scratch', 'scratch2', 'python3-thonny']
+
+  package{$development:
+    ensure  => absent
+  }
+}
